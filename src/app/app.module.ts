7b9133486627cb0710e4a12898374d8d4a16142d
ClementVaugoyeau <clement.vaugoyeau@gmail.com>
2023-04-27 16:45:44 +0200

    dressing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HeaderHomeComponent } from './commons/header-home/header-home.component';
+import { DressingComponent } from './commons/dressing/dressing.component';
+//Angular
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeaderHomeComponent,
+    DressingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    //Angular Material
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/commons/dressing/dressing.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dressing',
+  templateUrl: './dressing.component.html',
+  styleUrls: ['./dressing.component.css']
+})
+export class DressingComponent {
+
+  listClothes: any[] = [
+    {
+      id : 1,
+      img: "../assets/img/dressing/t-shirt-nike.png"
+    },
+    {
+      id : 2,
+      img: "../assets/img/dressing/t-shirt-nike.png"
+    },
+    {
+      id : 3,
+      img: "../assets/img/dressing/t-shirt-nike.png"
+    },
+    {
+      id : 4,
+      img: "../assets/img/dressing/t-shirt-nike.png"
+    }
+
+  ]
+
+ constructor(){}
+
+}
