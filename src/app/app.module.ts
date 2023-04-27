aadb6b6de899e6b2ca1de46c2e22b2f3f432b9ec
nseya <sainseya0x0@gmail.com>
2023-04-27 16:58:11 +0200

    nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
 
+//Import Angular Material
+import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+//Import component
+import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './commons/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    NavBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
