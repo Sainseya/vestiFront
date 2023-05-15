b64971247ffe55a8b36898dc36df96aa9d78fbb3
ClementVaugoyeau <clement.vaugoyeau@gmail.com>
2023-05-15 12:01:56 +0200

    adjust footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FooterHomeComponent } from './commons/footer-home/footer-home.component';
 
 
 
@@ -30,7 +31,8 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     HeaderHomeComponent,
     DressingComponent,
     CardClotheComponent,
-    OfdtComponent
+    OfdtComponent,
+    FooterHomeComponent
   ],
   imports: [
     BrowserModule,
