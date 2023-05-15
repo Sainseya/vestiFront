1652189cfe6bf58b049ed67f6e34e0eead635c94
mischa <mischafrangeul77@gmail.com>
2023-05-15 11:00:20 +0200

    adding Footer for merge later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,6 +19,6 @@ import { OfdtComponent } from './commons/odtd/ofdt.component';
   bootstrap: [AppComponent]
 })
 
-export class AppModule { 
+export class AppModule {
 
 }
