4583255e459f06e75cc77b91780de186af490b8e
nseya <sainseya0x0@gmail.com>
2023-05-17 13:30:39 +0200

    welcom-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {MatIconModule} from '@angular/material/icon';
 
 //Import Angular Material
 import { MatCardModule } from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {  MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 //Import component
 import { NotFoundComponent } from './commons/not-found/not-found.component';
 import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
 import { FooterComponent } from './commons/footer/footer.component';
+import { IphoneComponent } from './commons/iphone/iphone.component';
+import { BoxComponent } from './commons/box/box.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     NavBarComponent,
     FooterComponent,
+    IphoneComponent,
+    BoxComponent,
+    WelcomePageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
-
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
