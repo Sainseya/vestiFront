7f40803e42de6d3493c8f4480a363895ad2b1501
ClementVaugoyeau <clement.vaugoyeau@gmail.com>
2023-06-07 09:06:15 +0200

    darmode header

--- src/app/commons/header-home/header-home.component.ts
+++ src/app/commons/header-home/header-home.component.ts
@@ -10,9 +10,40 @@ export class HeaderHomeComponent {
 
   @Input() drawer:any;
 
+  darkmodeChecked :boolean = false;
+  element:any
+  element2:any
+  element3:any
+  element4:any
+
+ toggleDarkmode(){
+
+
+    this.element = document.body.querySelector('.container-header');
+    this.element2 = document.body.querySelector('.input-recherche');
+    this.element3 = document.body.querySelector('.burger-menu-button');
+    this.element4 = document.body.querySelector('.logo-header');
+
+    if(!this.darkmodeChecked){
+    this.element.classList.add('dark-theme')
+    this.element2.classList.add('dark-theme')
+    this.element3.classList.add('dark-theme')
+    this.element4.classList.add('logo-header-dark')
+
+    this.darkmodeChecked = !this.darkmodeChecked
+    }
+    else{
+      this.element.classList.remove('dark-theme')
+      this.element2.classList.remove('dark-theme')
+      this.element3.classList.remove('dark-theme')
+      this.element4.classList.remove('logo-header-dark')
+      this.darkmodeChecked = !this.darkmodeChecked
+    }
 
 
 
+  //  document.body.classList.toggle("light-mode");
+ }
 
 
 }
