3df37082e6c0e836ea9bd819205bc94c9f064718
mischa <mischafrangeul77@gmail.com>
2023-06-08 10:23:40 +0200

    fix : Refactor ofdt -> ootd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { EventPageComponent } from './pages/event-page/event-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { OfdtComponent } from './pages/odtd/ofdt.component';
+import { OotdComponent } from './pages/ootd-page/ootd.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 
@@ -10,7 +10,7 @@ const routes: Routes = [
 
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
-  {path: 'outfit-of-the-day', component: OfdtComponent},
+  {path: 'outfit-of-the-day', component: OotdComponent},
   {path: 'welcome-page', component: WelcomePageComponent},
   {path: 'event', component: EventPageComponent}
 
