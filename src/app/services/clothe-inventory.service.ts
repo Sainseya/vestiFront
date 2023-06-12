fa2517daf4f7b8cd7b3dd4fbcdca63dfe7ed478f
mischa <mischafrangeul77@gmail.com>
2023-06-12 15:14:23 +0200

    fix : application run : OK

--- src/app/services/clothe-inventory.service.ts
+++ src/app/services/clothe-inventory.service.ts
@@ -9,15 +9,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ClotheInventoryService {
-<<<<<<< HEAD
-
-  private URI = "http://localhost:8080"
-  userId: string = '648705859aaaaf554aae3be2'
-
-=======
   private URI = 'http://localhost:8080';
   userId: string = '6487112851012704f07baebd';
->>>>>>> a3545344f701f4acb78a2c929917fc377545a0fd
   // private userId: string = "";
 
   public setId(id: string): void {
@@ -26,31 +19,9 @@ export class ClotheInventoryService {
   constructor(private http: HttpClient) {}
 
   //Appelle la liste de d'utilisateur sur le serveur Spring
-<<<<<<< HEAD
-  getAll = () : Observable<IUsers[]> => {
-
-    return this.http.get<IUsers[]>(`${this.URI}/vesti`)
-  }
-
-  getByTypeTop = () : Observable<Item[]> => {
-    console.log
-    return this.http.get<Item[]>(`${this.URI}/vesti/${this.userId}/wardrobe/top`)
-  }
-
-  getByTypeBottom = () : Observable<Item[]> => {
-
-    return this.http.get<Item[]>(`${this.URI}/vesti/${this.userId}/wardrobe/bottom`)
-  }
-
-  getByTypeShoes = () : Observable<Item[]> => {
-
-    return this.http.get<Item[]>(`${this.URI}/vesti/${this.userId}/wardrobe/shoes`)
-  }
-=======
   getAll = (): Observable<IUsers[]> => {
     return this.http.get<IUsers[]>(`${this.URI}/vesti`);
   };
->>>>>>> a3545344f701f4acb78a2c929917fc377545a0fd
 
   getByTypeTop = (): Observable<Item[]> => {
     return this.http.get<Item[]>(
